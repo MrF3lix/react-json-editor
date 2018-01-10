5a3d440dffecdc61476f4cc5f7719b1107ed138c
MrF3lix <felix.saaro@me.com>
2018-01-10 09:25:44 +0100

    update index.js to ref correct pathes and added .babelrc

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import jsonEditorApp from './reducers'
-import App from './components/App'
+import jsonEditorApp from './src/public/reducers'
+import App from './src/public/components/App'
 
-let initialState = {
+
+const initialState = {
     textEditor: {
-        jsonText: "jsonText",
-        jsonObject: {}
+       jsonText: "{test}",
+       jsonObject: {text: "test"}
     }
 }
 
 let store = createStore(jsonEditorApp, initialState)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
