27a89cd9c8db93d8e729b4a32bbe33f7cf03c4fd
MrF3lix <felix.saaro@me.com>
2018-01-15 22:23:00 +0100

    if the content of a property is an object load the object aswell in the object editor

--- src/public/components/JsonElement.js
+++ src/public/components/JsonElement.js
@@ -2,6 +2,26 @@ import React from 'react'
 import TextElement from './TextElement'
 
 const JsonElement = ({objectKey, content, changeToEditMode, saveTextFromInput}) =>{
+
+    console.log(Object.keys(content))
+
+    if(typeof content === 'object')
+    {
+
+        let keys = Object.keys(content)
+
+        return(
+            <div className="json-element">
+                <h2>{objectKey}</h2>
+                <div className="json-child-element">
+                    {keys.map((key, i) => (
+                        <JsonElement key={i} objectKey={key} content={content[key]} changeToEditMode={() => changeToEditMode(i)} saveTextFromInput={event => saveTextFromInput(event, key)}/>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+
     return(
         <div className="json-element">
             <h2>{objectKey}</h2>
